2027fb483c1deb42153a058bec16c45e6e59fef7
Arthur <alencararthur47@gmail.com>
2026-02-27 20:12:31 -0300

    Criando type paciente e ajustando o restante do projeto.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Especialidade } from "./types/especialidade";
+import { Paciente } from "./types/paciente";
+import { StatusConsulta } from "./types/statusConsulta";
+import { Medico } from "./interfaces/medico";
+import { Consulta } from "./interfaces/consulta";
